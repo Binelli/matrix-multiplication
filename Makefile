72f5826f38019a9eb70946144ccd58f84e43d31d
Bruno Binelli <brunobinelli@gmail.com>
2021-09-18 17:29:20 -0300

    All tests in 1 file

--- Makefile
+++ Makefile
@@ -5,9 +5,9 @@ SRC_DIR=./src
 OBJ_DIR=obj
 BUILD_DIR=build
 
-TARGETS=$(BUILD_DIR)/matrix_1
+TARGETS=$(BUILD_DIR)/matrix_mult
 
-_OBJS = matrix_1.o
+_OBJS = matrix_mult.o
 OBJS = $(patsubst %,$(OBJ_DIR)/%,$(_OBJS))
 
 all: $(TARGETS)
@@ -18,7 +18,10 @@ $(OBJ_DIR):
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-$(BUILD_DIR)/matrix_1: $(OBJS) | $(BUILD_DIR)
+$(BUILD_DIR)/matrix_mult: $(OBJS) | $(BUILD_DIR)
+	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
+
+$(BUILD_DIR)/matrix_2: $(OBJS) | $(BUILD_DIR)
 	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
 
 $(BUILD_DIR):
---
+++ src/matrix_mult.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define n 4096
+double A[n][n];
+double B[n][n];
+double C[n][n];
+
+float tdiff(struct timeval *start, struct timeval *end) {
+    return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+void mult() {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void mult_chg_loop() {
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            for (int j = 0; j < n; ++j) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])  {
+    int test_perf = 1;
+
+    if (argc > 1) {
+        test_perf = atoi(argv[1]);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = (double)rand() / (double)RAND_MAX;
+	    B[i][j] = (double)rand() / (double)RAND_MAX;
+	    C[i][j] = 0.0;
+	}
+    }
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    switch (test_perf)
+    {
+        case 1:
+            mult();
+            break;
+        case 2:
+            mult_chg_loop();
+            break;
+        default:
+            printf("Test perf %d not implemented", test_perf);
+            break;
+    }        
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f", tdiff(&start, &end));
+    return 0;
+}
